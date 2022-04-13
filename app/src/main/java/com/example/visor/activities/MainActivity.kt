970ef5570a66f537d86619a7f7c52fcc611b5f7f
Elizabeth Capuzzi <elicasama@gmail.com>
2022-04-12 23:34:06 -0300

    Primer commit. Ya podemos ver un listado harcodeado de monedas y precios

---
+++ app/src/main/java/com/example/visor/activities/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.visor.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.visor.R
+import com.example.visor.api.CoinGeckoApi
+import com.example.visor.models.Moneda
+import j3.simple_recycler_adapter.SimpleRecyclerAdapter
+import j3.simple_recycler_adapter.setup
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_moneda.view.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
+
+class MainActivity : AppCompatActivity(), CoroutineScope {
+    override val coroutineContext: CoroutineContext get() = Dispatchers.Main
+
+    private val coinGeckoApi = CoinGeckoApi.new()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        configurarVista()
+    }
+
+    private fun configurarVista() {
+        // TODO: Usar coinGeckoApi para listar las monedas
+
+        val monedas = listOf(
+            Moneda(
+                nombre = "Moneda 1",
+                precioActual = 12f
+            ),
+            Moneda(
+                nombre = "Moneda 2",
+                precioActual = 22f
+            )
+        )
+
+
+        val adapter = SimpleRecyclerAdapter<Moneda>(R.layout.item_moneda) { view, moneda, _ ->
+            view.etiqueta_nombre.text = moneda.nombre
+            view.etiqueta_precio.text = moneda.precioActual.toString()
+        }
+
+        lista_monedas.setup(adapter)
+        adapter.populate(monedas)
+
+    }
+}
