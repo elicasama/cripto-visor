d3ce7c82645fd0b38fdebb3efd7c6a6333f23ddf
Elizabeth Capuzzi <elicasama@gmail.com>
2022-05-02 22:52:41 -0300

    se actualiza Kotest para poder realizar tests unitarios

--- app/src/test/java/com/example/visor/ExampleUnitTest.kt
+++ app/src/test/java/com/example/visor/ExampleUnitTest.kt
@@ -1,12 +1,13 @@
 package com.example.visor
 
+import com.example.visor.models.Moneda
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
 class ExampleUnitTest : DescribeSpec({
-    describe("Ejemplo") {
-        it("1 + 1 es 2") {
-            (1 + 1) shouldBe 2
+    describe("Moneda") {
+        it("estaEnAlza con variación positiva devuelve true") {
+            Moneda(variacionEn24hs = 2.0).estaEnAlza() shouldBe true
         }
 
         // TODO: Agregar tests acá
